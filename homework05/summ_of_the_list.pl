054b01cb0488653e870237f8b5c054332582cfb5
ChistilinMV <max.chistilin@gmail.com>
2023-10-03 14:54:51 +0300

    HomeWork05 minor changes

--- homework05/summ_of_the_list.pl
+++ homework05/summ_of_the_list.pl
@@ -4,4 +4,4 @@ summ_list([Head|Tail], Sum) :-
     summ_list(Tail, SumTmp), Sum is SumTmp + Head.
 
 % Query
-summ_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Sum)
+?- summ_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], Sum)
